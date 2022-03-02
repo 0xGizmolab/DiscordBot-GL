192896172d7371203d382f32a3148180eb31879f
Abbas Ali <abbasali.mehtar@gmail.com>
2022-03-02 22:03:15 +0530

    changed embed color

--- commands/roles/create.js
+++ commands/roles/create.js
@@ -10,7 +10,7 @@ const {
 
 module.exports = {
   name: "rr-create",
-  run: async(client, message, args) => {
+  run: async (client, message, args) => {
     let data = []
 
     let file = await readFile("./data.json", {
@@ -23,19 +23,19 @@ module.exports = {
     let channel = message.mentions.channels.first()
     if (!channel) return message.reply("Mention a channel!")
     if (!category) return message.reply("Mention a category!")
-    
+
     data = data.find(c => c.category === category)
-    
+
     if (!data) return message.reply("That category doesn't exists!")
 
     let embed = new MessageEmbed()
-    .setAuthor({
-      name: data.category, URL: "https://gizmolab.xyz"
-    })
-    .setThumbnail(message.guild.iconURL())
-    .setColor("cyan")
-    .setDescription("> Click buttons to get your role!")
-    .setURL('https://gizmolab.xyz')
+      .setAuthor({
+        name: data.category, URL: "https://gizmolab.xyz"
+      })
+      .setThumbnail(message.guild.iconURL())
+      .setColor("#2f3136")
+      .setDescription("> Click buttons to get your role!")
+      .setURL('https://gizmolab.xyz')
 
 
     let buttons = []
@@ -43,10 +43,10 @@ module.exports = {
 
     for (const role of data.roles) {
       const button = new MessageButton()
-      .setCustomId("br:" + role.role)
-      .setLabel(role.name)
-      .setEmoji(role.emoji ? role.emoji: null)
-      .setStyle("PRIMARY")
+        .setCustomId("br:" + role.role)
+        .setLabel(role.name)
+        .setEmoji(role.emoji ? role.emoji : null)
+        .setStyle("PRIMARY")
 
       buttons.push(button)
     }
@@ -57,8 +57,8 @@ module.exports = {
     rows.forEach((row, i) => {
       row.addComponents(buttons.slice(0 + (i * 5), 5 + (i * 5)));
     });
-    
-    channel.send({embeds: [embed], components: rows})
+
+    channel.send({ embeds: [embed], components: rows })
 
     message.reply("You can now add roles in " + category)
   }
